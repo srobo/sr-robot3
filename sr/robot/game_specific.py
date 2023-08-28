e4373760a87302901aaf588dc679680a94d3d1a0
WillB97 <will@rudi.org.uk>
2023-08-28 21:38:36 +0100

    Update marker sizes

--- sr/robot/game_specific.py
+++ sr/robot/game_specific.py
@@ -3,9 +3,8 @@ from __future__ import annotations
 
 from typing import Iterable
 
-# TODO: Update this for 2023
 # Marker sizes are in mm
 MARKER_SIZES: dict[Iterable[int], int] = {
-    range(28): 100,  # 0 - 27 for arena boundary
-    range(28, 100): 80,  # Everything else is a token
+    range(100): 150,  # arena boundaries
+    range(100, 200): 80,  # Everything else is a token
 }
